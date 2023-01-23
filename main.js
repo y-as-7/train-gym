fb27cfe1f7dce803ee9e545840fd72c93d6d4cea
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-01-23 10:53:07 +0200

    make my website responsive part1

--- main.js
+++ main.js
@@ -1,48 +1,36 @@
-
-let searchIcon =document.getElementById("search");
-let searchInput=document.querySelector(".nav2 div .search input");
+let searchIcon = document.getElementById("search");
+let searchInput = document.querySelector(".nav2 div .search input");
 
 searchIcon.onclick = () => {
   searchIcon.style.display = "none";
-  searchInput.style.display="block";
-  if (searchInput.style.display = "block") {
+  searchInput.style.display = "block";
+  if ((searchInput.style.display = "block")) {
     searchInput.focus();
   }
   searchInput.focus();
 
-if(searchInput.value==""){
+  if (searchInput.value == "") {
     setTimeout(() => {
-        searchIcon.style.display = "block";
-        searchInput.style.display = "none";
+      searchIcon.style.display = "block";
+      searchInput.style.display = "none";
     }, 25000);
-}
+  }
 };
-
-
 
 // animations to my website
 ScrollReveal({
-   reset: true ,
-   distance:'60px',
-   duration:2500,
-   delay:400
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
 });
 
-
-
 // animation to 3 cards
-ScrollReveal().reveal(".con-under-img div", { delay: 500, origin: "bottom" ,interval:200});
+ScrollReveal().reveal(".con-under-img div", {
+  delay: 200,
+  origin: "bottom",
+  interval: 200,
+});
 
 // animation to titles
-ScrollReveal().reveal(".first-con-about h1", { delay: 500 , origin:'left' });
-
-
-
-
-
-
-
-
-
-
-
+ScrollReveal().reveal(".first-con-about h1", { delay: 500, origin: "left" });
